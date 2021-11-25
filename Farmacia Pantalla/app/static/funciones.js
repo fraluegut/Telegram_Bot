6d31ac758526c59b0842b2afffb5280d34eb1371
Francisco Javier Luengo Gutiérrez <franciscojavierluengo@outlook.com>
2021-11-25 09:41:13 +0100

    Delete Farmacia Pantalla directory

--- Farmacia Pantalla/app/static/funciones.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-    function myFunction(){
-        if(!form_valid){
-            alert('Given data is not correct');
-            return false;
-        }
-        return true;
-    }
-</script>
