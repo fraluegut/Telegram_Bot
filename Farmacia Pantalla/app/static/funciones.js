50a6fc32c382696578aee4389d057be3f9f627ed
fraluegut <franciscojavierluengo@outlook.com>
2021-11-25 09:37:56 +0100

    Initial commit

---
+++ Farmacia Pantalla/app/static/funciones.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    function myFunction(){
+        if(!form_valid){
+            alert('Given data is not correct');
+            return false;
+        }
+        return true;
+    }
+</script>
